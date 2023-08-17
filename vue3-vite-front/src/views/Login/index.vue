027292e4a85f0489115b60dbf02da60dd227d4bb
whoelse <347300979@qq.com>
2023-08-17 22:37:39 +0800

    模拟登录

--- vue3-vite-front/src/views/Login/index.vue
+++ vue3-vite-front/src/views/Login/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login flex justify-center items-center w-full h-full">
     <div
-      class="login-form text-center p-10 w-3/5 h-96 rounded border-2 border-slate-200 border-solid"
+      class="login-form overflow-hidden text-center p-10 w-3/5 h-96 rounded border-2 border-slate-200 border-solid"
     >
       <h1 class="text-3xl font-bold mb-10">登录</h1>
       <el-form
@@ -9,26 +9,34 @@
         :model="ruleForm"
         status-icon
         :rules="rules"
-        label-width="120px"
         class="demo-ruleForm"
+        label-width="100px"
       >
-        <el-form-item label="Password" prop="pass">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model.number="ruleForm.username" />
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
           <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
         </el-form-item>
-        <el-form-item label="Confirm" prop="checkPass">
+        <el-form-item label="验证码" prop="code">
+          <el-input v-model="ruleForm.code" type="text" autocomplete="off" />
+        </el-form-item>
+        <!-- <el-form-item label="确认密码" prop="checkPass">
           <el-input
             autofocus
             v-model="ruleForm.checkPass"
             type="password"
             autocomplete="off"
           />
-        </el-form-item>
-        <el-form-item label="Age" prop="age">
-          <el-input v-model.number="ruleForm.age" />
-        </el-form-item>
+        </el-form-item> -->
+
         <el-form-item>
-          <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
-          <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+          <div class="w-full flex justify-center items-center">
+            <el-button type="primary" @click.prevent="handleLogin(ruleFormRef)"
+              >登录</el-button
+            >
+            <el-button @click.prevent="handleRegister(ruleFormRef)">注册</el-button>
+          </div>
         </el-form-item>
       </el-form>
     </div>
@@ -36,41 +44,50 @@
 </template>
 
 <script lang="ts" setup>
-import { login } from '@/api/login';
-import { reactive, ref } from 'vue';
-import type { FormInstance, FormRules } from 'element-plus';
+import { getCurrentInstance, reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+import { login, register } from '@/api/login';
+import { ElMessage, type FormInstance, type FormRules } from 'element-plus';
 const ruleFormRef = ref<FormInstance>();
-// 在模板中启用 v-focus
-const vFocus = {
-  mounted: (el: HTMLElement) => el.focus(),
-};
-const checkAge = (rule: any, value: any, callback: any) => {
+const { proxy } = getCurrentInstance();
+
+const checkCode = (rule: any, value: any, callback: any) => {
   if (!value) {
-    return callback(new Error('Please input the age'));
+    return callback(new Error('请输入验证码'));
   }
-  setTimeout(() => {
-    if (!Number.isInteger(value)) {
-      callback(new Error('Please input digits'));
-    } else {
-      if (value < 18) {
-        callback(new Error('Age must be greater than 18'));
-      } else {
-        callback();
-      }
-    }
-  }, 1000);
+  callback();
+};
+
+const checkUsername = (rule: any, value: any, callback: any) => {
+  if (!value) {
+    return callback(new Error('Please input the username'));
+  }
+  callback();
+  // setTimeout(() => {
+  //   if (!Number.isInteger(value)) {
+  //     callback(new Error('Please input digits'));
+  //   } else {
+  //     if (value < 18) {
+  //       callback(new Error('Age must be greater than 18'));
+  //     } else {
+  //       callback();
+  //     }
+  //   }
+  // }, 1000);
 };
 
 const validatePass = (rule: any, value: any, callback: any) => {
   if (value === '') {
     callback(new Error('Please input the password'));
-  } else {
+  } /*  else {
     if (ruleForm.checkPass !== '') {
       if (!ruleFormRef.value) return;
       ruleFormRef.value.validateField('checkPass', () => null);
     }
     callback();
-  }
+  }*/
+  callback();
 };
 const validatePass2 = (rule: any, value: any, callback: any) => {
   if (value === '') {
@@ -84,21 +101,44 @@ const validatePass2 = (rule: any, value: any, callback: any) => {
 
 const ruleForm = reactive({
   pass: '',
+  code: '',
   checkPass: '',
-  age: '',
+  username: ''
 });
 
 const rules = reactive<FormRules<typeof ruleForm>>({
   pass: [{ validator: validatePass, trigger: 'blur' }],
   checkPass: [{ validator: validatePass2, trigger: 'blur' }],
-  age: [{ validator: checkAge, trigger: 'blur' }],
+  username: [{ validator: checkUsername, trigger: 'blur' }],
+  code: [{ validator: checkCode, trigger: 'blur' }]
 });
 
-const submitForm = (formEl: FormInstance | undefined) => {
+const handleLogin = (formEl: FormInstance | undefined) => {
+  console.log('formEl', formEl === proxy.$refs.ruleFormRef);
+  // console.log('proxy.$refs.ruleFormRef', proxy.$refs.ruleFormRef);
+  //   proxy.$refs.ruleFormRef.validate((valid:any) => {
+  //   console.log('proxy - valid', valid);
+  // });
   if (!formEl) return;
-  formEl.validate((valid) => {
+  formEl.validate(valid => {
     if (valid) {
-      console.log('submit!');
+      // if (valid) { }
+      console.log('ruleForm', ruleForm);
+      if (ruleForm.username === 'admin' && ruleForm.pass === '111111' && ruleForm.code === '1111') {
+        ElMessage({
+          message: '登录成功!',
+          type: 'success'
+        });
+        setTimeout(function () {
+          router.push('/');
+        }, 2000);
+      } else {
+                  ElMessage({
+    message: '登录失败!',
+    type: 'error',
+  })
+        formEl.resetFields();
+      }
     } else {
       console.log('error submit!');
       return false;
@@ -106,7 +146,7 @@ const submitForm = (formEl: FormInstance | undefined) => {
   });
 };
 
-const resetForm = (formEl: FormInstance | undefined) => {
+const handleRegister = (formEl: FormInstance | undefined) => {
   if (!formEl) return;
   formEl.resetFields();
 };
